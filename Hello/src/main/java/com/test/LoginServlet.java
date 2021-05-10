5a69041d09877bbe0314ff8c400cc27a12f0b9aa
Do-yoon <cheeezing_u@naver.com>
2021-05-10 16:52:16 +0900

    adding data reader module

--- Hello/src/main/java/com/test/LoginServlet.java
+++ Hello/src/main/java/com/test/LoginServlet.java
@@ -25,12 +25,10 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String usel = request.getParameter("fruit");
-        String res = "<html><h3>Selected fruit: " + usel + "</h3></html>";
-        response.getWriter().print(res);
-    }
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
